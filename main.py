191bc5a73de502b0da19577f7ce788d69b07f1e6
Josh Hawkins <josh.hawkins119@gmail.com>
2017-02-09 16:46:04 -0600

    All done

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ page_header = """
     </style>
 </head>
 <body>
-    <h1>User Signup
+    <h1>
+    User Signup
     </h1>
 """
 page_footer = """
@@ -38,71 +39,49 @@ page_footer = """
 </html>
 """
 
-#def build(self, username = "", password = "", verified_password = "", email = "", error = ""):
-    #Set of strings containing all the parts of the page
-    #String will optionally add components that are passed in
-
-form_header = """
-<form action="/Welcome" method="post">"""
 
 
-add_username = """
+form  = """
+<form method="post">
     <label>
         Username
-        <input type="text" name="username" required>%(username)s</input>
-        <span class="error">%(error)s</span>
+        <input type="text" name="username" value="%(username)s"/>
     </label>
-"""
+    <div style="color: red">%(error1)s</div>
 
-
-add_password = """
     <label>
         Password
-        <input type="text" name="password" required>%(password)s</input>
-    <span class="error">%(error)s</span>
+        <input type="password" name="password" value=""/>
     </label>
-"""
+    <div style="color: red">%(error2)s</div>
 
-
-
-add_verified_password = """
     <label>
-        Password
-        <input type="text" name="verified_password" required>%(verified_password)s</input>
-        <span class="error">%(error)s</span>
+        Verify Password
+        <input type="password" name="match" value=""/>
     </label>
-"""
+    <div style="color: red">%(error3)s</div>
 
-
-add_email = """
     <label>
         Email (Optional)
-        <input type="text" name="email">%(email)s</input>
-        <span class="error">%(error)s</span>
+        <input type="text" name="email" value="%(email)s"/>
     </label>
+    <div style="color: red">%(error4)s</div>
+
+    <input type="submit" value="Submit"/>
+</form>
 """
 
-form_footer = """
-    <input type="submit" value"></input>
-    """
+content = page_header + form + page_footer
 
-    #content = page_header + form_header + add_username + add_password + add_verified_password + add_email + form_footer + page_footer
-    #return content
+def escape_html(s):
+    return cgi.escape(s, quote = True)
 
-    #self.response.out.write(content %{"username": cgi.escape(username), "password": cgi.escape(password), "verified_password": cgi.escape(verified_password), "email": cgi.escape(email)})
-#user_input = {:}
-
-
-
-user_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
-    return user_RE.match(username)
+    return USER_RE.match(username)
 
-
-def password_match(password):
-    if valid_password == True:
-        pass
-
+def password_match(match, password):
+    return match == password
 
 PASSWORD_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_password(password):
@@ -111,115 +90,85 @@ def valid_password(password):
 
 EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 def valid_email(email):
-    return EMAIL_RE.match(email)
+    if email:
+        return EMAIL_RE.match(email)
+    else:
+        return True
 
 
 
 
 
 class MainHandler(webapp2.RequestHandler):
-# sets up input boxes for Username, Password, Verify Password, and Email(Optional)
-    def build(self, username = "", password = "", verified_password = "", email = ""):
-        content = page_header + form_header + add_username + add_password + add_verified_password + add_email + form_footer + page_footer
-        self.response.out.write(content %{"username": cgi.escape(username), "password": cgi.escape(password), "verified_password": cgi.escape(verified_password), "email": cgi.escape(email)})
 
+    def build(self, username="", error1="", error2="", email="", error3="", error4=""):
 
+        self.response.out.write(content % {"username": username, "error1": error1, "error2": error2, "email": email, "error3": error3, "error4": error4})
 
 
     def get(self):
         self.build()
 
 
+    def post(self):
+        input_error = False
+        user_name = self.request.get("username")
+        user_password = self.request.get("password")
+        user_verified_password = self.request.get("match")
+        user_email = self.request.get("email")
 
 
+        username = valid_username(user_name)
+        password = valid_password(user_password)
+        email = valid_email(user_email)
+        match = password_match(user_password, user_verified_password)
+
+        if username and password and email and match:
+            self.redirect("/welcome?username=" + user_name)
+
+        else:
+
+            if not username:
+                error1 = "Invalid Entry. No spaces and must be 3-20 characters."
+            else:
+                error1 = ""
 
 
+            if not password:
+                error2 = "Invalid Entry. Must be 3-20 characters."
+            else:
+                error2 = ""
 
+
+            if not match:
+                error3 = "Invalid Entry. Passwords do not match."
+            else:
+                error3 = ""
+
+
+            if not email:
+                error4 = "Invalid Entry. Not a valid email address."
+            else:
+                error4 = ""
+
+            self.build(user_name, error1, error2, user_email, error3, error4)
 
 class Welcome(webapp2.RequestHandler):
 
+    def get(self):
 
+        username = self.request.get('username')
 
-    def post(self):
-        # dictionary = {'user' : self.request.get("username"),
-        # 'password' : self.request.get("password"),
-        # 'verified_password' : self.request.get("verified_password"),
-        # 'email' : self.request.get("email")
-        # }
+        if valid_username(username):
+            welcome_message = "<h1>" + "Welcome " + "<strong>" + username + "</strong>" + "!" + "</h1>"
+            self.response.write(welcome_message)
 
-
-        username = self.request.get("username")
-        password = self.request.get("password")
-        verified_password = self.request.get("verified_password")
-        email = self.request.get("email")
-
-
-
-
-        good_username = valid_username(username)
-        good_password = valid_password(password)
-        good_verified_password = password_match(verified_password)
-        good_email = valid_email(email)
-
-        # Two steps:
-        # first, is *anything* wrong? If not, send them to the welcome screen.
-        if good_username == True:
-            pass
         else:
-            error = "Invalid Entry. No spaces and must be 3-20 characters."
-            self.redirect("/?error=" + cgi.escape(error))
-
-        if good_password == True:
-            pass
-        else:
-            error = "Invalid Entry. Must be 3-20 characters."
-            self.redirect("/?error=" + cgi.escape(error))
-
-        if good_verified_password == True:
-            pass
-        else:
-            error = "Invalid Entry. Passwords do not match."
-            self.redirect("/?error=" + cgi.escape(error))
-
-        if good_email == True:
-            pass
-        else:
-            error = "Invalid Entry. Not a valid email address."
-            self.redirect("/?error=" + cgi.escape(error))
-
-        # if something *is* wrong, check each field in turn, and come up with
-        # an error message if that field needs work.
-
-        # Send them the build() form again, this time with error messages.
-
-        welcome_message = str(good_username)
-        welcome_message = "<h1>" + "Welcome " + "<strong>" + welcome_message + "</strong>" + "!" + "</h1>"
-        self.response.write(welcome_message)
-
-
-# The user does not enter a username
-# The user's username is not valid -- for example, contains a space character. Full spec is included in the notes underneath the video.
-# The user's password and password-confirmation do not match
-# The user provides an email, but it's not a valid email.
-
-## WHERE IS MY BUG?
-## HOW DO I USE THE DICTIONARY FOR FUNCTIONS?
-## HAVE MY CREDENTIALS GONE THROUGH USING THE SUCCESS CLASS?
-## HOW DO I USE ERROR MESSAGES CORRECTLY?
-
-
-
-class Success(webapp2.RequestHandler):
-    def success_input(self):
-        final_username = self.request.get(good_username)
-        final_password = self.request.get(good_password)
-        final_email = self.request.get(good_email)
-        self.redirect('/success?final_username=' + email + '&final_password=' + product)
+            self.redirect('/')
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/Welcome', Welcome),
-    ('/Success', Success)
+    ('/welcome', Welcome)
 
 ], debug=True)
